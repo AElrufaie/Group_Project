4fed44f50a4b42509ec54c10d1fb2abe6d573213
Ashraf Elrufaie <ashraf.elrufaie@mail.mcgill.ca>
2025-04-27 16:16:46 -0400

    added function for testing

--- src/modeling/model_training.py
+++ src/modeling/model_training.py
@@ -84,3 +84,12 @@ def train_decision_tree(X_train, y_train, best_params):
     
     mlflow_manager.end_run()
     return model
+
+# For Testing
+def train_model(X_train, y_train, X_val, y_val, params):
+    """Train a model and return it."""
+    from sklearn.ensemble import RandomForestClassifier
+    model = RandomForestClassifier(**params)
+    model.fit(X_train, y_train)
+    return model
+
