ed1e77781807392335326a06a40bbf003eab798a
Ashraf Elrufaie <ashraf.elrufaie@mail.mcgill.ca>
2025-04-28 16:20:30 -0400

    Create test_feature_engineering.py

---
+++ tests/preprocessing/test_feature_engineering.py
@@ -0,0 +1,24 @@
+from src.preprocessing.preprocessing import feature_engineering
+
+def test_feature_engineering():
+    animal_df = pd.DataFrame({
+        'animal_id': ['a1'],
+        'datetime_intake': pd.to_datetime(['2022-01-01T10:00:00']),
+        'datetime_outcome': pd.to_datetime(['2022-05-10T10:00:00']),
+        'name_intake': ['Doggo'],
+        'outcome_type': ['Adoption'],
+        'sex_upon_outcome': ['Neutered Male'],
+        'breed_intake': ['Labrador Mix'],
+        'color_intake': ['Black'],
+        'intake_condition': ['Normal'],
+        'date_of_birth_outcome': pd.to_datetime(['2020-01-01'])
+    })
+    
+    processed_df = feature_engineering(animal_df)
+
+    assert 'los_at_shelter' in processed_df.columns
+    assert processed_df['los_at_shelter'].iloc[0] == 9
+    assert 'outcome_group' in processed_df.columns
+    assert processed_df['outcome_group'].iloc[0] == 'Positive'
+    assert 'has_name' in processed_df.columns
+    assert processed_df['has_name'].iloc[0] == 1
