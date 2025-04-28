6a13062ce15eb7dd12bda25ede145cccb6feba7d
Ashraf Elrufaie <ashraf.elrufaie@mail.mcgill.ca>
2025-04-28 16:30:21 -0400

    Create test_NAN_labels.py

---
+++ tests/clustering/test_NAN_labels.py
@@ -0,0 +1,22 @@
+def test_no_nan_in_clusters():
+    df = pd.DataFrame({
+        'los_at_shelter': np.random.randint(1, 30, 50),
+        'age_days_outcome': np.random.randint(100, 5000, 50),
+        'animal_type': np.random.choice(['Dog', 'Cat'], 50),
+        'has_name': np.random.choice([0, 1], 50),
+        'age_group_intake': np.random.choice(['Puppy/Kitten', 'Adult'], 50),
+        'month_of_outcome': np.random.choice(['January', 'February'], 50),
+        'is_fixed': np.random.choice([0, 1], 50),
+        'breed_type': np.random.choice(['Mix', 'Pure'], 50),
+        'color_group': np.random.choice(['Black', 'White'], 50),
+        'intake_condition_group': np.random.choice(['Healthy/Normal', 'Injured'], 50),
+    })
+
+    numerical_features = ["los_at_shelter", "age_days_outcome"]
+    categorical_features = ["animal_type", "has_name", "age_group_intake", 
+                             "month_of_outcome", "is_fixed", "breed_type", 
+                             "color_group", "intake_condition_group"]
+
+    clusters, model = run_kprototypes(df, numerical_features, categorical_features, n_clusters=3)
+
+    assert not any(pd.isnull(clusters))
